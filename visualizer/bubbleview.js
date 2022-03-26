7df4acd186711278d2a494034a6f6b20d264821d
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2022-03-26 13:06:36 +0100

    finish bubbleview code and create first set of codecharts

--- visualizer/bubbleview.js
+++ visualizer/bubbleview.js
@@ -1,8 +1,8 @@
-async function codechart({img, width, height}) {
+async function bubbleview({img, width, height}) {
   console.log('test');
   let base = d3.select('#image');
 
-  let all_data = await d3.csv('./../data/vcd-codecharts.csv');
+  let all_data = await d3.csv('./../data/vcd-bubbleview.csv');
   console.log(all_data[0]);
 
   let xAccessor = d => +d.x;
@@ -29,9 +29,30 @@ async function codechart({img, width, height}) {
     .attr('width', width)
     .attr('height', height);
 
-  // 3. create x and y scales
+  // 3. create x and y scales and color scale
   let xScale = d3.scaleLinear().domain([0, width]).range([0, width]);
   let yScale = d3.scaleLinear().domain([0, height]).range([0, height]);
+  let color = d3
+    .scaleOrdinal()
+    .domain(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'])
+    .range([
+      '#00429d',
+      '#3761ab',
+      '#5681b9',
+      '#73a2c6',
+      '#93c4d2',
+      '#b9e5dd',
+      '#ffffe0',
+      '#ffd3bf',
+      '#ffa59e',
+      '#f4777f',
+      '#dd4c65',
+      '#be214d',
+      '#93003a',
+    ]);
+
+  let a = data[0];
+  console.log(data.map(d => d.participant));
 
   // 4. draw dots on image
   let dots = container
@@ -41,9 +62,9 @@ async function codechart({img, width, height}) {
     .append('circle')
     .attr('cx', d => xScale(xAccessor(d)))
     .attr('cy', d => yScale(yAccessor(d)))
-    .attr('r', 10)
-    .attr('opacity', 0.7)
-    .attr('fill', '#ffffff');
+    .attr('r', d => 50)
+    .attr('opacity', 0.8)
+    .attr('fill', d => color(d.participant));
 
   // do not erase after this
 }
