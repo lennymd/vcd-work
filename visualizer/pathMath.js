b040459bf0fe5609ee8b4da23a42a70e928e2d9e
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2022-04-03 19:13:20 +0200

    work with data

--- visualizer/pathMath.js
+++ visualizer/pathMath.js
@@ -1,6 +1,10 @@
 async function pathMath(img) {
   let all_data = await d3.csv('./../data/vcd-bubbleview.csv');
 
+  let container = d3.select('#container');
+
+  let section = container.append('div').append('h1').text(img);
+
   let xAccessor = d => +d.x;
   let yAccessor = d => +d.y;
   let imgAccessor = d => d.image;
@@ -9,5 +13,52 @@ async function pathMath(img) {
     .filter(d => imgAccessor(d) == `${img}.jpeg`)
     .filter(d => d.x != 'N/A');
 
-  console.log(data);
+  const participants = [
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+  ];
+
+  participants.forEach(p => {
+    let _data = data.filter(d => d.participant == p);
+
+    let pathLength = [];
+
+    for (let i = 1; i < _data.length; i++) {
+      let p1 = _data[i - 1];
+      let p2 = _data[i];
+
+      // find deltaX between p1 and p2
+      let deltaX = xAccessor(p2) - xAccessor(p1);
+      let deltaY = yAccessor(p2) - yAccessor(p1);
+
+      let _distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+
+      pathLength.push(_distance);
+    }
+
+    // sum all path segmentes
+    let totalDistance = d3.sum(pathLength);
+
+    // find mean path segment
+    let meanSegment = d3.mean(pathLength);
+
+    // find median path segment
+    let medianSegment = d3.median(pathLength);
+
+    container
+      .append('p')
+      .text(
+        `${p}, ${img}, ${_data.length}, ${totalDistance}, ${meanSegment}, ${medianSegment}`
+      );
+  });
 }
